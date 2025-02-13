128ccf74865b37bdf371bb2792b2f7bbb8e4af26
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-02-13 01:57:06 +0100

    Feat: Add some mobile responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <section className="text-center flex flex-col items-center justify-center min-h-[80vh] px-4">
+        <section className="text-center flex flex-col items-center justify-center min-h-[80vh] px-4 py-10 sm:py-12">
             <img src="/assets/Logomain.png" alt="Rebalancr Logo" className="h-40 sm:h-48 mb-6 sm:mb-12" />
             <h2 className="text-3xl sm:text-4xl font-semibold mb-4 sm:mb-6">
                 Automate Your Token Swaps with{" "}
@@ -21,12 +21,12 @@ const Home = () => {
                 <br />
                 Seamlessly swap tokens and automate your trading with ease.
             </p>
-            <div className="mt-6 flex flex-col sm:flex-row gap-4 sm:gap-6">
-                <button className="bg-[#00F5A0] text-[#0D0F1F] px-6 py-3 sm:px-8 sm:py-4 font-semibold transition-all duration-200 transform hover:scale-110 hover:bg-[#00D495] cursor-pointer text-sm sm:text-base">
+            <div className="mt-6 flex gap-6">
+                <button className="bg-[#00F5A0] text-[#0D0F1F] px-8 py-4 font-semibold transition-all duration-200 transform hover:scale-110 hover:bg-[#00D495] cursor-pointer text-sm sm:text-base">
                     Get Started
                 </button>
                 <Link to="/features">
-                    <button className="border border-[#E0E0E0] text-[#E0E0E0] px-6 py-3 sm:px-8 sm:py-4 font-semibold transition-all duration-200 transform hover:scale-110 hover:bg-[#0D0F1F] hover:text-[#00F5A0] cursor-pointer text-sm sm:text-base">
+                    <button className="border border-[#E0E0E0] text-[#E0E0E0] px-8 py-4 font-semibold transition-all duration-200 transform hover:scale-110 hover:bg-[#0D0F1F] hover:text-[#00F5A0] cursor-pointer text-sm sm:text-base">
                         Know More
                     </button>
                 </Link>
